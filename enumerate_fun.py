0cfda3b5905e3267a98be7c79ebfea74900402c5
digrawal <68938821+digrawal@users.noreply.github.com>
2020-07-29 11:40:29 +0530

    Add files via upload

---
+++ enumerate_fun.py
@@ -0,0 +1,12 @@
+l1 = ["raj","sunil","anil","anita"]
+#i = 1
+#for item in l1:
+#    if i%2 != 0:
+ #       print(item)
+  #  i += 1
+
+#we can print this with enumerate function
+
+for index, item in enumerate(l1):
+    if index%2 != 0:
+        print(item)
